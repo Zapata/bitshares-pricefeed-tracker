8b6a4ab596e11f17c9db07937722561db217281f
Zapata <marco.tessari@gmail.com>
2018-10-15 01:27:16 +0200

    Dynamically adjust CEX bucket size.

--- bitshares_pricefeed_tracker/util.py
+++ bitshares_pricefeed_tracker/util.py
@@ -19,6 +19,28 @@ _cex_pairs = {
     'CNY': 'bts_qc'
 }
 
+_zb_buckets = [
+    (  60,  '1min' ),
+    ( 180,  '3min' ),
+    ( 300,  '5min' ),
+    ( 900, '15min' ),
+    ( 1800, '30min' ),
+    ( 3600, '1hour'),
+    ( 7200, '2HOUR'),
+    ( 14400, '4hour'),
+    ( 21600, '6hour'),
+    ( 43200, '12hour'),
+    ( 86400, '1day'),
+    ( 259200, '3day'),
+    ( 604800, '1week')
+]
+
+_zb_max_returned_elements = 200
+
+def _compute_zb_bucket_size(start_date):
+    date_range = datetime.utcnow() - dateutil.parser.parse('{} UTC'.format(start_date)).replace(tzinfo=None)
+    return next( (bucket[1] for bucket in _zb_buckets if date_range.total_seconds() / bucket[0] < _zb_max_returned_elements), _zb_buckets[-1][1])
+
 def has_cex_prices(asset):
     return bool(asset in _cex_pairs)
 
@@ -27,8 +49,10 @@ def cex_price_source(asset):
 
 def get_cex_prices(asset, start_date):
     start_timestamp = int(dateutil.parser.parse('{} UTC'.format(start_date)).timestamp() * 1e3)
+    bucket = _compute_zb_bucket_size(start_date)
+    url = 'http://api.zb.cn/data/v1/kline?market={}&type={}&since={}'.format(_cex_pairs[asset], bucket, start_timestamp)
     response = requests.get(
-        url='http://api.zb.cn/data/v1/kline?market={}&type=15min&since={}'.format(_cex_pairs[asset], start_timestamp),
+        url=url,
         headers=_request_headers,
         timeout=5).json()
     if 'data' not in response:
